8bfcfff14f5905c8c58a6de8a879a1598cd3e5cd
Allie Tran <ld.allie.tran@gmail.com>
2025-09-11 17:12:42 +0100

    Fix     auto_capture.sh to add status messages

--- auto_capture.sh
+++ auto_capture.sh
@@ -2,14 +2,17 @@
 
 while ! rpicam-still --output starting.jpg
 do
-	sleep 1
+    echo "Waiting for camera to be ready..."
+    sleep 1
 done
 
+echo "Camera is ready. Starting timelapse capture."
 TIMEOUT=0
 TIMELAPSE=30 # in seconds
 DATE=$(date +"%Y-%m-%d")
 OUTPUT="Camera/timelapse/$DATE"
 
+echo "Saving images to $OUTPUT"
 test -d $OUTPUT || mkdir -p $OUTPUT
 start_index=$(ls $OUTPUT | wc -l)
-rpicam-still --timeout $TIMEOUT --timelapse $((TIMELAPSE * 1000)) -o $OUTPUT/image_%05d.jpg -n -w 1920 -h 1080 --framestart $start_index --rotation 180
+rpicam-still --timeout $TIMEOUT -o $OUTPUT/image_%05d.jpg -n -w 1920 -h 1080 --framestart $start_index --rotation 180 --timelapse $(($TIMELAPSE * 1000))
