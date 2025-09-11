bbe8bdeb2d11ee9260d16547f1e86e9011eecf84
Allie Tran <allie.tran@dcu.ie>
2025-09-11 13:59:49 +0100

    Initial commit

---
+++ auto_capture.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+while ! rpicam-still --output starting.jpg
+do
+	sleep 1
+done
+
+TIMEOUT=0
+TIMELAPSE=30 # in seconds
+DATE=$(date + "%Y-%m-%d_%H%M")
+OUTPUT="Camera/timelapse/$DATE"
+
+test -d $OUTPUT || mkdir -p $OUTPUT
+rpicam-still --raw --timeout $TIMEOUT --timelapse $((TIMELAPSE * 1000)) -o $OUTPUT/%04d.jpg
