dd0593e2f9d273f84ac439e4dbfce059061a0f92
Allie Tran <ld.allie.tran@gmail.com>
2025-09-22 10:08:14 +0100

    Add retry logic to monitor.sh for uploading files

--- monitor.sh
+++ monitor.sh
@@ -5,18 +5,45 @@
 # Configuration
 DATE=$(date +"%Y-%m-%d")
 DIR="Camera/timelapse/$DATE"
+LOG_FILE="Camera/logs/$DATE.log"
 
 # REMOTE_URL="https://dcu.allietran.com/omi/be/upload-image"
 REMOTE_URL="https://mysceal.computing.dcu.ie/omi/be/upload-image"
+
 
 # function to send file to remote server
 send_file() {
     local file_path="$1"
     timestamp=$(date -r "$file_path" +"%s")
     timestamp=$((timestamp * 1000))
-    echo "Sending file: $file_path with timestamp: $timestamp"
-    curl -X PUT -F "file=@${file_path}" -F "timestamp=${timestamp}" "$REMOTE_URL"
+    # sending file with retry logic
+    while true; do
+        response=$(curl -s -o /dev/null -w "%{http_code}" -X PUT -F "file=@${file_path}" -F "timestamp=${timestamp}" "$REMOTE_URL")
+        if [ "$response" -eq 200 ]; then
+            echo "File $file_path sent successfully."
+            echo "$file_path" >> "$LOG_FILE"
+            break
+        else
+            echo "Failed to send $file_path. HTTP status code: $response. Retrying in 5 seconds..."
+            sleep 5
+        fi
+    done
 }
+
+
+# back up other folders that are not today
+for folder in Camera/timelapse/*; do
+    if [ -d "$folder" ] && [ "$(basename "$folder")" != "$DATE" ]; then
+        log_date=$(basename "$folder")
+        log_file="Camera/logs/$log_date.log"
+        for file in "$folder"/*; do
+            if ! grep -q "^$file$" "$log_file"; then
+                send_file "$file"
+            fi
+        done
+    fi
+done
+
 
 # Monitor the directory for new files
 echo "Watching $DIR"
