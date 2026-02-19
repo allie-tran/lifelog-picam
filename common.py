1f82acc1d9fd3763a563f9dd3b1ee6aff48ee68a
Allie Tran <ld.allie.tran@gmail.com>
2026-02-19 10:54:12 +0000

    Add encryption to camera

--- common.py
+++ common.py
@@ -4,29 +4,40 @@ from datetime import datetime
 import requests
 from dotenv import load_dotenv
 
-load_dotenv()
-device_id = os.getenv("DEVICE_ID", "omi")
+import nacl.utils
+from nacl.public import PrivateKey, Box, PublicKey
 
-BACKEND_URL = "https://dcu.allietran.com/omi/be"
-UPLOAD_URL = "https://dcu.allietran.com/omi/be/upload-image"
-UPLOAD_VIDEO_URL = "https://dcu.allietran.com/omi/be/upload-video"
-CHECK_URL = "https://dcu.allietran.com/omi/be/check-image-uploaded"
-CHECK_ALL_URL = "https://dcu.allietran.com/omi/be/check-all-images-uploaded"
+DEVICE_SECRET_KEY = os.getenv("DEVICE_SECRET_KEY", "")
+SERVER_PUBLIC_KEY = os.getenv("SERVER_PUBLIC_KEY", "")
+assert DEVICE_SECRET_KEY and SERVER_PUBLIC_KEY, "Both DEVICE_SECRET_KEY and SERVER_PUBLIC_KEY environment variables must be set."
+box = Box(PrivateKey(bytes.fromhex(DEVICE_SECRET_KEY)), PublicKey(bytes.fromhex(SERVER_PUBLIC_KEY)))
+
+load_dotenv()
+device_id = os.getenv("DEVICE_ID", "allie")
+
+BACKEND_URL = "https://dcu.allietran.com/selfhealth/be"
+UPLOAD_URL = f"{BACKEND_URL}/upload-image"
+UPLOAD_VIDEO_URL = f"{BACKEND_URL}/upload-video"
+CHECK_URL = f"{BACKEND_URL}/check-image-uploaded"
+CHECK_ALL_URL = f"{BACKEND_URL}/check-all-images-uploaded"
 OUTPUT = "Camera/timelapse"
+
+IMAGE_EXTENSION = ".jpg"
 
 def send_image(image_path, uploaded_files, LOG_FILE):
     if image_path in uploaded_files:
         return "photo"
 
     timestamp = datetime.strptime(
-        os.path.basename(image_path).replace(".jpg", ""), "%Y%m%d_%H%M%S"
+        os.path.basename(image_path).replace(IMAGE_EXTENSION, ""), "%Y%m%d_%H%M%S"
     )
     timestamp = int(timestamp.timestamp() * 1000)
 
     # Send form-data request
     with open(image_path, "rb") as img_file:
+        # the file is encrypted, so we don't need to encrypt it again. Just send it as is.
         files = {
-            "file": (os.path.basename(image_path), img_file, "image/jpeg"),
+            "file": (os.path.basename(image_path), img_file, f"image/jpeg"),
             "timestamp": (None, str(timestamp)),
         }
         response = requests.put(UPLOAD_URL, files=files, headers={"X-Device-ID": device_id})
