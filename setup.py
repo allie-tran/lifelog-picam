2c800b69480d5dde901df11e38d37309428379d3
Allie Tran <ld.allie.tran@gmail.com>
2026-06-05 09:07:15 +0100

    Change code for generate uuid

--- setup.py
+++ setup.py
@@ -1,25 +1,22 @@
 """
 Script for installing the device.
 """
-
 from nacl.public import PrivateKey
 import subprocess
 import os
+import uuid
 
 def get_id():
-    if 'nt' in os.name:
-        return subprocess.Popen('dmidecode.exe -s system-uuid'.split())
-    else:
-        return subprocess.Popen('hal-get-property --udi /org/freedesktop/Hal/devices/computer --key system.hardware.uuid'.split())
+    return str(uuid.uuid5(uuid.NIL, os.uname().nodename))
 
 local_sk = PrivateKey.generate()
 local_pk = local_sk.public_key
 
 # write the public key to a file
 with open(".env", "a") as f:
+    f.write(f"DEVICE_ID={get_id()}\n")
     f.write(f"DEVICE_PUBLIC_KEY={local_pk.encode().hex()}\n")
     f.write(f"DEVICE_PRIVATE_KEY={local_sk.encode().hex()}\n")
-    f.write(f"DEVICE_ID={get_id()}\n")
 
 print(f"Public key: {local_pk.encode().hex()}")
 print(f"Private key: {local_sk.encode().hex()}")
